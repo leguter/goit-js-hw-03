02464efc3694d0582441f1bd4aaa786da1a4b67a
leguter <kolalevcenko80@gmail.com>
2024-04-20 09:07:28 -1200

    done

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+function slugify(title) {
+    return title.replaceAll(' ','-').toLowerCase();
+}
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
